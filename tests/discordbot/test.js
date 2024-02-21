6b8b5730ef3a3e2954c4b5d5476766d0df40efea
blackNoir2 <109542044+blackNoir2@users.noreply.github.com>
2024-02-21 14:22:18 +0000

    Update test.js
    Created a test to check if the bot actually logs on to the server.

--- tests/discordbot/test.js
+++ tests/discordbot/test.js
@@ -1,21 +1,12 @@
 // import test from "node:test"
 const test = require('node:test');
-
 const assert = require('assert');
 const dotenv = require("dotenv").config();
-
 const BotManager = require('../../src/BotManager.js');
-
 
 const DISCORD_BOT_TOKEN = dotenv.parsed.DISCORD_BOT_TOKEN;
 const SERVER_ID = dotenv.parsed.SERVER_ID;
 const OPEN_AI_KEY = dotenv.parsed.OPEN_AI_KEY;
-
-
-
-// Mock Discord client
-
-
 
 /**
  * Represents a test suite for the BotManager class.
@@ -36,40 +27,25 @@ class BotManagerTestSuite {
         this.discordBot = new BotManager(DISCORD_BOT_TOKEN, SERVER_ID, OPEN_AI_KEY);
     }
 
-    /**
-  * Tests the successful initialization of the bot.
-  */
-    async testBotInitializationSuccess() {
-        try {
-            // Mock the Discord client
-            const mockClient = {
-                user: {
-                    displayName: "MockUser"
-                },
-                login: function () {
-                    // Simulate login success
-                    return Promise.resolve();
-                },
-                once: function (event, callback) {
-                    // Simulate the Ready event
-                    if (event === 'ready') {
-                        callback();
-                    }
-                }
-            };
 
+    /**
+     * Tests the successful initialization of the bot.
+     */
+    async testBotInitializationSuccess() {
+
+        // Mock Discord client
+        try {
             // Replace the real client with the mock client
-            this.discordBot._client = mockClient;
+            this.discordBot._client = this._mockClient;
 
             // Call the login method
             await this.discordBot.login();
+            const isInitialized = this.discordBot._initialized;
 
             // Assert that the bot is initialized
-            if (!this.discordBot._initialized) {
-                throw new Error("[-] Bot initialization failed");
-            }
+            assert.strictEqual(isInitialized, true, "[+] Bot initialization successful");
 
-            console.log("[+] Bot initialization successful");
+
         } catch (error) {
             console.error("[-] Bot initialization failed:", error.message);
         }
@@ -91,8 +67,8 @@ class BotManagerTestSuite {
     }
 
     /**
-    * Tests bot initialization with an empty Discord bot token.
-    */
+     * Tests bot initialization with an empty Discord bot token.
+     */
     async testBotInitializationWithEmptyDisbotToken() {
         const discordBot = new BotManager("", SERVER_ID, OPEN_AI_KEY);
 
@@ -104,26 +80,95 @@ class BotManagerTestSuite {
         }
     }
 
+    /**
+     * Test if a new Discord bot successfully logs into the server and announces its presence.
+     * This test ensures that the bot manager correctly handles the login process and sets up
+     * the bot's presence message upon successful login.
+     */
+    async testSuccesfulBotLogin() {
+        // Create a new instance of the BotManager with valid credentials
+        const testDiscordBot = new BotManager(DISCORD_BOT_TOKEN, SERVER_ID, OPEN_AI_KEY);
+
+
+        await testDiscordBot.login();
+
+        testDiscordBot.setBotname("Test Bot");
+
+        const botName = testDiscordBot.botName;
+
+        // Create the expected greeting message based on the bot's name that will be shown on the server
+        const expectedGreetingMsg = `Hello everyone! I'm the **${botName}**, now online and ready to chat. To chat with me, ` +
+            `**type @${botName} followed by your prompt**. ` +
+            `To see your history, **type @${botName} ${testDiscordBot._showHistoryCommand}** ` +
+            `and to send a **DM (direct message)** to the user type **@<username>  followed by your message**`;
+
+        try {
+            const resp           = await testDiscordBot._announcePresence();
+            const annoucementMsg = resp.content;
+
+            testDiscordBot.logout();
+
+            assert.strictEqual(annoucementMsg, expectedGreetingMsg, "[+] The test shows the correct greeting when successful log into discord");
+
+        } catch (error) {
+            // If an error occurs during login or announcement, fail the test
+            assert.fail(`[-] The bot failed to log into Discord, Error - ${error}`);
+        }
+    }
+
+    /**
+     * Create a mock client
+     */
+    get _mockClient() {
+        return {
+            user: {
+                displayName: "MockUser"
+            },
+            login: function () {
+                // Simulate login success
+                return Promise.resolve();
+            },
+            once: function (event, callback) {
+                // Simulate the Ready event
+                if (event === 'ready') {
+                    callback();
+                }
+            }
+        }
+    }
 
     /**
      * Runs all tests in the suite.
      */
     async runTests() {
-        test.describe("testBotInitializationSuccess", () => {
-            test("The bot should be initialized after login", () => this.testBotInitializationSuccess());
-        })
-      
-        test.describe("testBotInitializationWithInvalidDisbotToken", () => {
-            test("The bot shouldn't initialize with incorrect login details", () => this.testBotInitializationWithInvalidDisbotToken());
 
-        })
+        try {
+            await test.describe("testSuccesfulBotLogin", () => {
+                test("Once logged into the discord server the bot should greet the user with a message", () => this.testSuccesfulBotLogin());
 
-        test.describe("testBotInitializationWithEmptyDisbotToken", () =>  {
-            test("The bot shouldn't initialize with an empty discord bot token", () => this.testBotInitializationWithEmptyDisbotToken());
-        });
-       
+            });
+            test.describe("testBotInitializationSuccess", () => {
+                test("The bot should be initialized after login", () => this.testBotInitializationSuccess());
+            });
+
+            test.describe("testBotInitializationWithInvalidDisbotToken", () => {
+                test("The bot shouldn't initialize with incorrect login details", () => this.testBotInitializationWithInvalidDisbotToken());
+            });
+
+            test.describe("testBotInitializationWithEmptyDisbotToken", () => {
+                test("The bot shouldn't initialize with an empty discord bot token", () => this.testBotInitializationWithEmptyDisbotToken());
+            });
+
+        } catch (error) {
+            console.log("[-] Something went wrong with the test with running the tests!!")
+        } finally {
+            this.tearDown();
+        }
+
+
     }
+
+
 }
 
 module.exports = BotManagerTestSuite;
-
