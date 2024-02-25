2ae936fae2ec239e1561a91a0f67cc9074e9fd01
blackNoir2 <109542044+blackNoir2@users.noreply.github.com>
2024-02-25 03:21:40 +0000

    implemented the message event listener simulation
    Implemented message event listener test and added negative test cases
    
    - Added a test to simulate receiving a message and validate that the bot responds with a "hello" message. This test is designed to ensure that the message event listener functions correctly and the bot replies appropriately to incoming messages.
    
    - Introduced negative test cases to validate scenarios where the bot fails to display the expected message upon login or produces unexpected responses to incoming messages. These negative tests aim to identify potential issues and enhance the robustness of the bot's handling logic.

--- tests/discordbot/test.js
+++ tests/discordbot/test.js
@@ -1,14 +1,16 @@
 // import test from "node:test"
-const test = require('node:test');
-const assert = require('assert');
-const dotenv = require("dotenv").config();
-
-
+const test       = require('node:test');
+const assert     = require('assert');
+const sinon      = require('sinon');
+const dotenv     = require("dotenv").config();
 const BotManager = require('../../src/BotManager.js');
 
+const { parseUserMentionAndMessage } = require("../../src/utils.js")
+
 const DISCORD_BOT_TOKEN = dotenv.parsed.DISCORD_BOT_TOKEN;
-const SERVER_ID = dotenv.parsed.SERVER_ID;
-const OPEN_AI_KEY = dotenv.parsed.OPEN_AI_KEY;
+const SERVER_ID         = dotenv.parsed.SERVER_ID;
+const OPEN_AI_KEY       = dotenv.parsed.OPEN_AI_KEY;
+
 
 /**
  * Represents a test suite for the BotManager class.
@@ -19,6 +21,7 @@ class BotManagerTestSuite {
      */
     constructor() {
         this.botManager = null;
+        this._dateNow = new Date();
         this.setup();
     }
 
@@ -32,29 +35,50 @@ class BotManagerTestSuite {
     tearDown() {
         this.discordBot.logout();
     }
+
     /**
-     * Tests the successful initialization of the bot.
-     */
+    * Tests the successful initialization of the bot.
+    * 
+    * This test verifies that the bot instance is properly initialized and logged in successfully.
+    * 
+    * The bot instance is expected to be in an initialized state after a successful login operation.
+    */
     async testBotInitializationSuccess() {
 
-        // Mock Discord client
         try {
-            // Replace the real client with the mock client
-            this.discordBot._client = this._mockClient;
 
-            // Call the login method
             await this.discordBot.login();
-            const isInitialized = this.discordBot._initialized;
+            await this.discordBot.logout();
 
-            // Assert that the bot is initialized
-            assert.strictEqual(isInitialized, true, "[+] Bot initialization successful");
-
+            assert.strictEqual(this.discordBot._initialized, true, "[+] Bot initialization successful");
 
         } catch (error) {
-            console.error("[-] Bot initialization failed:", error.message);
+            assert.fail(`[-] The discord bot didn't initialize - something went wrong!!`);
         }
     }
 
+    /**
+    * Tests the scenario when the bot is not initialized.
+    * 
+    * This test verifies that the bot instance is not initialized, meaning it has not been properly 
+    * initialized and logged in.
+    * 
+    * The bot instance is expected to be in an uninitialized state 
+    * if it has not been logged in successfully.
+    */
+    async testBotWhenNotInitialized() {
+
+        try {
+
+            // The this.discordBot.login() is not called so the discordBot is never initalized to true
+
+            // Assert that the bot is initialized is equal to false
+            assert.strictEqual(this.discordBot._initialized, false, "[+] [Success] - The bot didn't initialized without the login method be called");
+
+        } catch (error) {
+            assert.fail("[-] The discord bot shouldn't be initialize without the login method being called. Something went wrong!!");
+        }
+    }
 
     /**
      * Tests bot initialization with an invalid Discord bot token.
@@ -72,15 +96,22 @@ class BotManagerTestSuite {
 
     /**
      * Tests bot initialization with an empty Discord bot token.
-     */
+    */
     async testBotInitializationWithEmptyDisbotToken() {
-        const discordBot = new BotManager("", SERVER_ID, OPEN_AI_KEY);
 
+        const EMPTY_DISCORD_TOKEN = ""
+        const discordBot = new BotManager(EMPTY_DISCORD_TOKEN, SERVER_ID, OPEN_AI_KEY);
+    
         try {
             await discordBot.login();
-            assert.fail("Expected the test to fail but the test succeeded");
+
+            // If no error is thrown then this means that bot was logged in with an empty string
+            // If that happens fail the test
+            assert.fail("Successful login in with an empty discord bot token should not be possible!!")
         } catch (error) {
-            assert.ok(error instanceof Error, 'BotManager initialization should throw an error with an empty token.');
+
+            assert.strictEqual(discordBot._initialized, false, "Success - the bot didn't login with an empty token")
+
         }
     }
 
@@ -89,22 +120,20 @@ class BotManagerTestSuite {
      * This test ensures that the bot manager correctly handles the login process and sets up
      * the bot's presence message upon successful login.
      */
-    async testSuccesfulBotLogin() {
+    async testSuccessfulGreetingMsgAfterLogin() {
 
-        // Create a new instance of the BotManager with valid credentials
         const testDiscordBot = new BotManager(DISCORD_BOT_TOKEN, SERVER_ID, OPEN_AI_KEY);
 
         await testDiscordBot.login();
 
         testDiscordBot.setBotname("Test Bot");
-
         const botName = testDiscordBot.botName;
 
         // Create the expected greeting message based on the bot's name that will be shown on the server
         const expectedGreetingMsg = `Hello everyone! I'm the **${botName}**, now online and ready to chat. To chat with me, ` +
             `**type @${botName} followed by your prompt**. ` +
             `To see your history, **type @${botName} ${testDiscordBot._showHistoryCommand}** ` +
-            `and to send a **DM (direct message)** to the user type **@<username>  followed by your message**`;
+            `and to send a **DM (direct message)** to the user type **@<username>  followed by your message**`
 
         try {
             const resp           = await testDiscordBot._announcePresence();
@@ -116,62 +145,283 @@ class BotManagerTestSuite {
 
         } catch (error) {
             // If an error occurs during login or announcement, fail the test
-            assert.fail(`[-] The bot failed to log into Discord, Error - ${error}`);
+            assert.fail(`[-] The announcement greeted by the Bot didn't match the expected repsonse`);
         }
     }
 
     /**
-     * Create a mock client
+     * Tests the scenario where the greeting message after bot login is not as expected.
+     * This is a negative test case.
      */
-    get _mockClient() {
+    async testUnsuccessfulGreetingMsgAfterLogin() {
+
+        const testDiscordBot = new BotManager(DISCORD_BOT_TOKEN, SERVER_ID, OPEN_AI_KEY);
+        await testDiscordBot.login();
+
+        // Create an unexpected greeting message
+        const unexpectedGreetingMsg = `Unexpected greeting message`;
+
+        try {
+            
+            const resp            = await testDiscordBot._announcePresence();
+            const annoucementMsg = resp.content;
+
+            testDiscordBot.logout();
+
+            // Assert that the announcement message is not equal to the unexpected greeting message
+            assert.notStrictEqual(annoucementMsg, unexpectedGreetingMsg, "[+] The test should not show the unexpected greeting message after successful login");
+
+        } catch (error) {
+            // If an error occurs during login or announcement, fail the test
+            assert.fail(`[-] The announcement greeted by the Bot matched the unexpected greeting message`);
+        }
+        }
+
+    
+    /**
+      * Tests the response event message listener functionality of the bot.
+      * 
+      * This test verifies that the bot's message listener responds correctly to incoming messages.
+      * It sends a test message to the bot and checks if the response matches the expected message.
+      * 
+      * The expected message is defined as "Hello".
+    */
+    async testBotResponseEventMessageListener() {
+
+        const expectedMessage = "Hello";
+        const response        = await this.testBotResponseEventMessageHelperFunction();
+        const messageContent  = parseUserMentionAndMessage(response.content).messageContent;
+
+        try {
+
+            // If the returned message content matches the expected Message - pass the test
+            assert.strictEqual(messageContent.toLowerCase(), expectedMessage.toLowerCase(), "This test event message listener is working");
+
+        } catch (error) {
+            assert.fail(`The event message listener did not respond with the expected message. Expected: "${expectedMessage}", Received: "${messageContent}".`);
+        }
+
+    }
+
+    /**
+     * Tests the response event message listener functionality of the bot to ensure it does not produce unexpected messages.
+     * 
+     * This test verifies that the bot's message listener does not respond with unexpected messages.
+     * It sends a test message to the bot and checks if the response does not match the unexpected message.
+     * 
+     * The unexpected message to check against is defined as "Unexpected message".
+     */
+    async testBotResponseEventMessageListener_UnexpectedMessage() {
+
+        const unExpectedMessage = "Unexpected message";
+        const response          = await this.testBotResponseEventMessageHelperFunction();
+
+        try {
+
+            const messageContent = parseUserMentionAndMessage(response.content).messageContent;
+
+            // Check that the received message does not match the unexpected message
+            assert.notDeepStrictEqual(unExpectedMessage, messageContent, 
+                    "Success - The unexpected message didn't match with the expected text received from the message event handler");
+
+        } catch (error) {
+            assert.fail(`Something went wrong the text shouldn't have matched: "${unExpectedMessage}".`);
+        }
+    }
+
+    /**
+     * Simulates the event message handling process of the bot in a test environment.
+     * This function creates a mock message, logs in the bot, stubs the necessary methods
+     * to execute custom logic, simulates message handling, and then logs out the bot.
+     * After handling the message, it restores the mocked methods to their original state
+     * and returns the response received from the bot.
+     * 
+     * @returns {Promise<Object>} A promise that resolves to the response received from the bot.
+     *
+     * @remarks
+     * This function is a helper function used by the `testBotResponseEventMessageListener_UnexpectedMessage`
+     * and `testBotResponseEventMessageListener` test cases to simulate the event message handling process
+     * of the bot.
+     */
+    async testBotResponseEventMessageHelperFunction() {
+        const message = this._mockMessage;
+        const testBot = new BotManager(DISCORD_BOT_TOKEN, SERVER_ID, "OPEN_AI_KEY");
+
+        await testBot.login();
+
+        const stubbedModerateUserPrompt = sinon.stub(testBot, '_moderateUserPrompt');
+        const stubbedOnMessageCreate = sinon.stub(testBot, '_onMessageCreate');
+
+        // Set up the stubbedOnMessageCreate to execute custom logic
+        stubbedOnMessageCreate.callsFake(async (message) => {
+
+            const mockResponse = await this._mockOnMessageCreate(message);
+            return mockResponse;
+        });
+
+        const response = await testBot._onMessageCreate(message);
+
+        await testBot.logout();
+
+        // restore the methods mocked back to their original state
+        stubbedOnMessageCreate.restore();
+        stubbedModerateUserPrompt.restore()
+
+        return response;
+    }
+
+    
+    /**
+     * Mocks the behavior of the `_onMessageCreate` method in the BotManager class.
+     * This method is used to simulate the response of the bot when a message is created.
+     * @param {object} message - The message object representing the received message.
+     * @returns {Promise<object>} A promise that resolves to the response message object.
+     */
+    async _mockOnMessageCreate(message) {
+        const authorID                = message.author.id;
+        const content                 = message.content;
+        const isDirectMessage         = false;
+        const clientApplicationMockID = "4444";
+
+        if (authorID !== clientApplicationMockID) {
+            return await this._mockModerateUserPrompt(content, message, isDirectMessage);
+        }
+
+    }
+
+    /**
+     * Mocks the behavior of the `_moderateUserPrompt` method in the BotManager class.
+     * This method is used to simulate the moderation process for a user message.
+     * @param {string} content - The content of the message.
+     * @param {object} message - The message object.
+     * @param {boolean} isDirectMessage - Indicates if the message is a direct message.
+     * @returns {string|object} A string indicating the success message or an object representing a received message.
+     */
+    async _mockModerateUserPrompt(content, message, isDirectMessage) {
+        const { userId, messageContent } = parseUserMentionAndMessage(content);
+
+        if (isDirectMessage) {
+            //TODO - Add functionality to do something later for now just return a string
+            return "The message has been successfully sent";
+        } else if (userId && messageContent && message) {
+
+            return this._mockRecievedMessage;
+        }
+    }
+
+    /**
+     * Returns a mock message object representing a received message.
+     * @returns {object} A mock message object.
+    */
+    get _mockRecievedMessage() {
         return {
-            user: {
-                displayName: "MockUser"
+            channelId: 'mock channel id',
+            guildId: 'mock guild id',
+            id: 'mock id',
+            createdTimestamp: this._dateNow,
+            type: 0,
+            system: false,
+            content: '<@733473713210785812> Hello',
+
+            application: {
+                id: "mock application id",
             },
-            login: function () {
-                // Simulate login success
-                return Promise.resolve();
+
+            author: {
+                id: 'mock author id',
+                bot: true,
             },
-            once: function (event, callback) {
-                // Simulate the Ready event
-                if (event === 'ready') {
-                    callback();
+
+            mentions: {
+                has: (userID) => {
+                    return userID === "mock User ID";
                 }
             }
         }
     }
 
     /**
-     * Runs all tests in the suite.
+     * Generates a mock message object for testing purposes.
+     * 
+     * @returns {Object} A mock message object with predefined properties.
      */
-    async runTests() {
-        console.log("[+] Starting tests, please wait....")
-        try {
-            await test.describe("testSuccesfulBotLogin", () => {
-                test("Once logged into the discord server the bot should greet the user with a message", () => this.testSuccesfulBotLogin());
+    get _mockMessage() {
 
-            });
-            test.describe("testBotInitializationSuccess", () => {
-                test("The bot should be initialized after login", () => this.testBotInitializationSuccess());
-            });
+        return {
+            channelId: '1210569481391448066',
+            guildId: '1210569480749457428',
+            id: '1210613247498002442',
+            createdTimestamp: this._dateNow,
+            type: 0,
+            system: false,
+            content: '<@733473713210785812> what are you?',
 
-            test.describe("testBotInitializationWithInvalidDisbotToken", () => {
-                test("The bot shouldn't initialize with incorrect login details", () => this.testBotInitializationWithInvalidDisbotToken());
-            });
-
-            test.describe("testBotInitializationWithEmptyDisbotToken", () => {
-                test("The bot shouldn't initialize with an empty discord bot token", () => this.testBotInitializationWithEmptyDisbotToken());
-            });
-
-        } catch (error) {
-            console.log("[-] Something went wrong with running the tests!!")
-        } finally {
-            this.tearDown();
-           
+            author: {
+                id: 'mock author id',
+                bot: false,
+            }
         }
-
-
     }
+
+/**
+ * Runs all tests in the suite.
+ */
+async runTests() {
+    console.log("[+] Starting tests, please wait....")
+    try {
+        // Successful greeting after login
+        await test.describe("testSuccessfulGreetingMsgAfterLogin", async () => {
+            await test("Once logged into the discord server the bot should greet the user with a message",
+                () => this.testSuccessfulGreetingMsgAfterLogin());
+        });
+
+        // Negative test case unsuccessful greeting after login
+        await test.describe("testUnsuccessfulGreetingMsgAfterLogin", async () => {
+            await test("The test verifies if the bot fails to display the expected message upon login",
+                () => this.testUnsuccessfulGreetingMsgAfterLogin());
+        });
+
+        // Test when the bot is not initialized
+        await test.describe("testBotWhenNotInitialized", () => {
+            test("Tests the scenario when the bot is not initialized properly",
+                () => this.testBotWhenNotInitialized());
+        });
+
+        // Test bot initialization success
+        await test.describe("testBotInitializationSuccess", async () => {
+            await test("The bot should be initialized after login", () => this.testBotInitializationSuccess());
+        });
+
+        // Test bot initialization with an invalid Discord token
+        test.describe("testBotInitializationWithInvalidDisbotToken", () => {
+            test("The bot shouldn't initialize with incorrect login details",
+                () => this.testBotInitializationWithInvalidDisbotToken());
+        });
+
+        // Test bot initialization with an empty Discord token
+        test.describe("testBotInitializationWithEmptyDisbotToken", () => {
+            test("The bot shouldn't initialize with an empty discord bot token",
+                () => this.testBotInitializationWithEmptyDisbotToken());
+        });
+
+        // Test bot response event message listener
+        await test.describe("testBotResponseEventMessageListener", async () => {
+            test("The bot message handler should responding with a hello after we send it a response", async () => {
+                await this.testBotResponseEventMessageListener();
+            });
+        });
+
+        // Test bot response event message listener with unexpected message
+        await test.describe("testBotResponseEventMessageListener_UnexpectedMessage", async () => {
+            test("The bot should match a response that does not equal the response returned from the event handler", async () => {
+                await this.testBotResponseEventMessageListener_UnexpectedMessage();
+            });
+        });
+
+    } catch (error) {
+        console.log("[-] Something went wrong with running the tests!!")
+    }
+}
 
 
 }
