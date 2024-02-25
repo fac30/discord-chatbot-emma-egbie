b87c752fbf52de6f5fcd1bce0843e14ea8bdfb72
Emma <65368545+electricblues@users.noreply.github.com>
2024-02-25 23:08:28 +0000

    tests for _getUserChats

--- tests/openAi.test.js
+++ tests/openAi.test.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv").config();
 const { test, describe, beforeEach } = require("node:test");
 const OpenAiManager = require("../src/OpenAiManager");
 
+const RANDOM_KEY = "123";
+
 describe("environment variables", () => {
   test("has values for the necessary environment variables", () => {
     const OPEN_AI_KEY = dotenv.parsed.OPEN_AI_KEY;
@@ -17,7 +19,7 @@ describe("getUserHistory", () => {
   let openAiManager;
 
   beforeEach(() => {
-    openAiManager = new OpenAiManager("1234");
+    openAiManager = new OpenAiManager(RANDOM_KEY);
   });
 
   test("returns an empty list when there's no user history", () => {
@@ -25,29 +27,79 @@ describe("getUserHistory", () => {
     assert.deepEqual(messages, []);
   });
 
-  test("returns the user prompts & replies if available", () => {
+  describe("existing user", () => {
     const userName = "userName";
     const testPrompt = "testPrompt";
     const testResponse = "testResponse";
 
-    openAiManager._cache = { [userName]: { [testPrompt]: testResponse } };
+    test("returns the user prompts & replies if available", () => {
+      openAiManager._cache = { [userName]: { [testPrompt]: testResponse } };
 
-    const messages = openAiManager.getUserHistory(userName);
-    assert.deepEqual(messages, [[testPrompt, testResponse]]);
-  });
+      const messages = openAiManager.getUserHistory(userName);
+      assert.deepEqual(messages, [[testPrompt, testResponse]]);
+    });
 
-  test("doesn't return the wrong user's chats", () => {
-    const userName = "userName";
-    const testPrompt = "testPrompt";
-    const testResponse = "testResponse";
+    test("doesn't return the wrong user's chats", () => {
+      openAiManager._cache = { [userName]: { [testPrompt]: testResponse } };
 
-    openAiManager._cache = { [userName]: { [testPrompt]: testResponse } };
-
-    const messages = openAiManager.getUserHistory("faulty user name");
-    assert.deepEqual(messages, []);
+      const messages = openAiManager.getUserHistory("faulty user name");
+      assert.deepEqual(messages, []);
+    });
   });
 });
 
-// describe("_getUserChats", () => {});
+describe("_getUserChats", () => {
+  const userName = "userName";
+  const testPrompt = "testPrompt";
+  const testResponse = "testResponse";
 
-// describe("_constructMessage", () => {});
+  /** @type {OpenAiManager} */
+  let openAiManager;
+
+  beforeEach(() => {
+    openAiManager = new OpenAiManager(RANDOM_KEY);
+
+    openAiManager._cache = { [userName]: { [testPrompt]: testResponse } };
+  });
+
+  test("returns an empty string when no user is provided", () => {
+    const userChats = openAiManager._getUserChats();
+    assert.deepEqual(userChats, "");
+  });
+
+  test("returns an empty string when non-existant user is provided", () => {
+    const userChats = openAiManager._getUserChats("faulty user");
+    assert.deepEqual(userChats, "");
+  });
+
+  test("returns the past user chats as a string", () => {
+    const userChats = openAiManager._getUserChats(userName);
+    assert.ok(userChats.includes(testPrompt));
+    assert.ok(userChats.includes(testResponse));
+  });
+
+  test("the userChats contains the appropriate number of Q: and A: ", () => {
+    const userChats = openAiManager._getUserChats(userName);
+
+    const historyLength = Object.keys(openAiManager._cache[userName]).length;
+
+    const qCount = (userChats.match(new RegExp("Q: ", "g")) || []).length;
+    const aCount = (userChats.match(new RegExp("A: ", "g")) || []).length;
+    assert.ok(qCount >= historyLength);
+    assert.ok(aCount >= historyLength);
+  });
+});
+
+// describe("_constructMessage", () => {
+//   /** @type {OpenAiManager} */
+//   let openAiManager;
+
+//   beforeEach(() => {
+//     openAiManager = new OpenAiManager("1234");
+//   });
+
+//   test("there are the correct number of system, assistant and user messages for a given user prompt", () => {
+//     const userChats
+
+//   });
+// });
